d486f97310c7794cf2306b22100b266f6cab5396
MathDotSqrt <ctrenkov@gmail.com>
2020-11-13 16:06:59 -0500

    Added glm

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include "header.h"
 #include <GLFW/glfw3.h>
+#include <gl/GL.h>
+#include <glm/glm.hpp>
 
 int main() {
+	glm::vec3 test(1, 2, 3);
 	glfwInit();
 	std::cout << "Hello World: " << LOA::getReturn() << '\n';
 	return 0;
