7c61cae8b636f4398afdf740c463efbe9d174b9e
MathDotSqrt <ctrenkov@gmail.com>
2020-11-15 10:45:11 -0500

    now builds with glew. Multiple build options

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1,14 +1,66 @@
-#pragma once
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
-#include "header.h"
+#include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
-#include <gl/GL.h>
 #include <glm/glm.hpp>
 
-int main() {
-	glm::vec3 test(1, 2, 3);
-	glfwInit();
-	std::cout << "Hello World: " << LOA::getReturn() << '\n';
-	return 0;
+using namespace glm;
+
+#define GLSL(src) #src
+
+static void error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+static void render(GLFWwindow* window) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+int main(void) {
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1, 0, 0, 1);
+        render(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        //glfwWaitEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
