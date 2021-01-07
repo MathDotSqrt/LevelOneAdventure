5f8c84023937e084f5c92b4d3abe59ca4d5924dc
Daniel Alther <dtalther1@gmail.com>
2021-01-07 18:27:35 -0500

    Player rotate smoothly with mouse

--- src/source/Systems/MouseCast.cpp
+++ src/source/Systems/MouseCast.cpp
@@ -22,40 +22,42 @@ void MouseCast::init() {
 	//reg.emplace<Component::Velocity>(ents,glm::vec3(6,2,4));
 	
 }
-
+/*https://www.youtube.com/watch?v=DLKN0jExRIM&t=302s Used this video to figure out how to convert the coords of the mouse to world coordinate.
+Basically it does the reverse of rendering a cube but stops at world space. Goes from viewport->normalized device-> clip space->eye space -> world space.*/
 void MouseCast::update(float delta) {
-	
 	LOA::Window &window = Window::getInstance();
-	glm::vec2 mouse = window.getMousePos();
+	glm::vec2 mouse = window.getMousePos();// Gets viewport space mouse coords
 	LOA::Physics::PhysicsScene &pscene = engine.getPhysicsScene();
 	entt::entity player = engine.getPlayer();
 	entt::registry& reg = engine.getRegistry();
-	
 	Graphics::Scene &scence = engine.getScene();
 	LOA::Graphics::PerspectiveCamera cam = scence.getMainCamera();
 	LOA::Graphics::BasicRenderer &render = engine.getRenderer();
-	float x = (2.0f*mouse.x)/render.getCurrentWidth()-1.0f;
+
+	float x = (2.0f*mouse.x)/render.getCurrentWidth()-1.0f;//Normalized Device space conversion
 	float y = (2.0f*mouse.y)/render.getCurrentHeight()-1.0f;
 	
-	glm::vec4 clipcoords = glm::vec4(x, -y, -1.0f, 1.0f);
+	glm::vec4 clipcoords = glm::vec4(x, -y, -1.0f, 1.0f);//Clip coords
 	glm::mat4 invertprojection = glm::inverse(render.getProjection());
 	glm::vec4 eyeCoords = invertprojection * clipcoords;
-	eyeCoords = glm::vec4(eyeCoords.x, eyeCoords.y, -1.0f, 0.0f);
+	eyeCoords = glm::vec4(eyeCoords.x, eyeCoords.y, -1.0f, 0.0f);//Eye coords, -1 is so it goes "into" the screen
 	glm::mat4 invertview = glm::inverse(cam.transform);
 	glm::vec4 rayworld = invertview * eyeCoords;
 	glm::vec3 mouseray = glm::vec3(rayworld.x, rayworld.y, rayworld.z);
 	mouseray = glm::normalize(mouseray);
 	
-	
-	
+	/*Get camera position and cast a ray using bullet 3 from the camera to the world coords of the mouse location.*/
 	glm::vec3 campos = reg.get<Component::Transformation>(engine.getMainCamera()).pos;
+	//The pair is a bool and a vec3. The bool is if it collided and the vec3 is where the closest collision was. Can use to click on terrain.
 	auto pair = pscene.castRay(campos, campos + mouseray * 600.0f, true);
-	
+
+	/*Found the angle between the forward vector of the player and the vector between the player and the mouse. Creates a quaternian using that angle 
+	and sets the player rotation to that new rotation. Use vec2 since we dont care about the y-axis and it repsects the sign of the angle. */
 	auto &playertrans = reg.get<Component::Transformation>(engine.getPlayer());
 	auto &playerdir = reg.get<Component::Direction>(engine.getPlayer());
 	glm::vec2 playervec = glm::vec2(pair.second.x - playertrans.pos.x, pair.second.z - playertrans.pos.z);
 	glm::vec2 forvec = glm::vec2(playerdir.forward.x , playerdir.forward.z );
-	float angle = glm::orientedAngle(glm::normalize(forvec), glm::normalize(playervec));//glm::normalize(glm::vec3(0,1,0)));
+	float angle = glm::orientedAngle(glm::normalize(forvec), glm::normalize(playervec));
 	glm::quat newrot = glm::angleAxis(-angle, glm::vec3(0, 1, 0));
 	playertrans.rot = newrot;
 }
