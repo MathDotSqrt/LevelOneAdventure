5d7002aec70dd834c0dc6a6b2b0113af95dc5148
Daniel Alther <dtalther1@gmail.com>
2021-01-07 18:13:29 -0500

    Rotate no comment

--- src/source/Systems/MouseCast.cpp
+++ src/source/Systems/MouseCast.cpp
@@ -7,10 +7,10 @@
 #include "glm/gtx/vector_angle.hpp"
 using namespace LOA::Systems;
 using namespace entt;
-static entt::entity dwagon;
+//static entt::entity dwagon;
 void MouseCast::init() {
 	//printf("We got printf out here.");
-	Graphics::Scene &scence = engine.getScene();
+	/*Graphics::Scene &scence = engine.getScene();
 	scence.loadMesh("Dwagon"_hs,"res/models/dragon/dragon2.stl");
 	Graphics::BasicLitMaterial material;
 	material.color = glm::vec3(.1,.76,.4);
@@ -18,25 +18,25 @@ void MouseCast::init() {
 	entt::registry& reg = engine.getRegistry();
 	dwagon = reg.create();
 	reg.emplace<Component::Transformation>(dwagon, glm::vec3(1, 1, 1), glm::angleAxis(3.14f/2*3.14f,glm::vec3(1,0,0)), glm::vec3(.1, .1, .1));
-	reg.emplace<Component::Renderable>(dwagon,id);
+	reg.emplace<Component::Renderable>(dwagon,id);*/
 	//reg.emplace<Component::Velocity>(ents,glm::vec3(6,2,4));
 	
 }
 
 void MouseCast::update(float delta) {
-	//printf("Still priunting");
+	
 	LOA::Window &window = Window::getInstance();
 	glm::vec2 mouse = window.getMousePos();
 	LOA::Physics::PhysicsScene &pscene = engine.getPhysicsScene();
 	entt::entity player = engine.getPlayer();
 	entt::registry& reg = engine.getRegistry();
-	//Component::Transformation trans = reg.get<Component::Transformation>(player);
+	
 	Graphics::Scene &scence = engine.getScene();
 	LOA::Graphics::PerspectiveCamera cam = scence.getMainCamera();
 	LOA::Graphics::BasicRenderer &render = engine.getRenderer();
 	float x = (2.0f*mouse.x)/render.getCurrentWidth()-1.0f;
 	float y = (2.0f*mouse.y)/render.getCurrentHeight()-1.0f;
-	//mouse = glm::vec2(x, -1.0f*y);
+	
 	glm::vec4 clipcoords = glm::vec4(x, -y, -1.0f, 1.0f);
 	glm::mat4 invertprojection = glm::inverse(render.getProjection());
 	glm::vec4 eyeCoords = invertprojection * clipcoords;
@@ -45,15 +45,12 @@ void MouseCast::update(float delta) {
 	glm::vec4 rayworld = invertview * eyeCoords;
 	glm::vec3 mouseray = glm::vec3(rayworld.x, rayworld.y, rayworld.z);
 	mouseray = glm::normalize(mouseray);
-	Component::Transformation &trans = reg.get<Component::Transformation>(dwagon);
 	
-	//printf("%f|%f|%f\n", mouseray.x, mouseray.y, mouseray.z);
+	
+	
 	glm::vec3 campos = reg.get<Component::Transformation>(engine.getMainCamera()).pos;
 	auto pair = pscene.castRay(campos, campos + mouseray * 600.0f, true);
-	/*if (pair.first)
-		trans.pos = pair.second;
-	else
-		trans.pos = campos + mouseray * 600.0f;*/
+	
 	auto &playertrans = reg.get<Component::Transformation>(engine.getPlayer());
 	auto &playerdir = reg.get<Component::Direction>(engine.getPlayer());
 	glm::vec2 playervec = glm::vec2(pair.second.x - playertrans.pos.x, pair.second.z - playertrans.pos.z);
@@ -61,9 +58,4 @@ void MouseCast::update(float delta) {
 	float angle = glm::orientedAngle(glm::normalize(forvec), glm::normalize(playervec));//glm::normalize(glm::vec3(0,1,0)));
 	glm::quat newrot = glm::angleAxis(-angle, glm::vec3(0, 1, 0));
 	playertrans.rot = newrot;
-	printf("%f\n",angle);
-	//printf("%d",pscene.castRay(campos, campos + mouseray * 600.0f,true));
-	
-	
-	//pscene.castRay(glm::vec3(mouse,0),trans.pos,true);
 }
